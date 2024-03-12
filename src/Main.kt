6a0bbd723f16735732b195bda89d2160c486723a
emobilis <phillionairephil@gmail.com>
2024-03-12 08:21:22 +0300

    kotlin

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World!")
+
+    println("New line")
+
+    morning()
+
+
+}
+
+fun morning(){
+    println("this is the morning function")
+}
---
+++ src/variables.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    var name: String = "Mark"
+    var math: Int = 80
+    var english: Double = 80.9
+    var kisw: Char = 'y'
+    var chem: Boolean = true
+    var his: Double = 99.9
+    english = 70.0
+
+    println(english)
+    println(math)
+    println(name)
+    println(kisw)
+    println(chem)
+    println(his)
+}
